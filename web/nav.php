ef6d9f1ed4e606a8934ee6d67576cc998bf73804
Rachel Chang <rjchang96@gmail.com>
2018-09-27 17:01:20 -0600

    login.php

--- web/nav.php
+++ web/nav.php
@@ -1,17 +1,17 @@
 <?php
 $file = basename($_SERVER['PHP_SELF']);
 
-if($file == "about.php")
+if($file === "about.php")
    echo "<div><a href = 'about.php' class = 'active'>About</a></div>";
 else
     echo "<div><a href = 'about.php'>About</a></div>";
 
-if($file == "login.php")
+if($file === "login.php")
    echo "<div><a href = 'login.php' class = 'active'>Login</a></div>";
 else
    echo "<div><a href = 'login.php'>Login</a></div>";
 
-if($file == "home.php")
+if($file === "home.php")
    echo "<div><a href = 'home.php' class = 'active'>Home</a></div>";
 else
     echo "<div><a href = 'home.php'>Home</a></div>";
