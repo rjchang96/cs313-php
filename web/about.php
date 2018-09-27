e8830f986c09d290853939b5cc6cb06f3bfe5c6c
Rachel Chang <rjchang96@gmail.com>
2018-09-27 15:56:35 -0600

    about.php

--- web/about.php
+++ web/about.php
@@ -4,10 +4,81 @@
    <meta charset="utf-8">
    <title>About us</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel = "style" href= "homeStyle.css">
+<style type="text/css">
+   body
+   {
+      background-image: url("blowup.jpg");
+   }
+</style>
 </head>
 <body>
-<header>Destroy your own Mountatin, Inc.</header>
+<div class = "grid-container">
+    <div class = "head">
+  <header> <!-- <img src = "images/07pics/HogwartsCrest.png" alt= "Hogwarts logo"
+    class = "logo" style="float:left" > -->
 
+    <h1 style ="font-size: 45;">Destory your own Mountain</h1>
+
+  </header>
+</div>
+
+<div class = "menu">
+    <!--<div class = "sidenav">-->
+    <div><a href = "about.php">About</a></div>
+    <br>
+   <div> <a href= "home.php">Home </a></div>
+   <br>
+  <div> <a href="login.php">Login</a></div>
+  <br>
+
+</div>
+<!-- <div class = "right">
+   <img src = "images/07pics/Nimbus_2000.jpg" alt = "Broomstick" style = " text-align: center;
+   width:200px; height:auto; max-width: 100%;">
+   <p style=" font-size: 20px;">
+    Nimbus 2000 for sale at Shacklebolt Brooms, the same broom stick
+    that Harry Potter used in Quidditch practice! Only $6,000 Galleons!
+   </p>
+   <img src = "images/07pics/hippogriff.jpg" alt = "hippogriff"
+   style = " text-align: center; width: 200px; height: auto;
+   max-width:100%">
+   <p style=" font-size: 20px;">
+    Buy your own Hippogriff from BaggleBogs Creatures! 50% off sale
+    only for $14,000 Galleons! Hurry while supplies last!
+   </p>
+  </div> -->
+
+<div class = "main" style=" font-size: 20px;">
+ <p>
+  <div style = "font-size: 20px;">
+   Are you tired of your mountain? With our enviromentally unconscious materials, we promise a quick and loud removal of all mole hills.
+   Those mountains will leave your life in a bang!
+  <!-- <b>Happiness</b> can be found in the
+   <br> darkest of times, <br> if one only remembers to <br>
+  turn on the <b>light</b> <br>
+  <i>-Albus Dumbledore</i>
+ </div> <br>
+  <img src = "images/07pics/Hogwarts.jpg" alt = "Hogwarts castle" style = " text-align: center;
+  width:800px; height:auto; max-width: 100%;">
+  <br> <br>
+  Every student at Hogwarts is placed into their houses according to their internal character
+  typically revealing by the sorting hat. Due to the sorting hat's recent retirement, we have
+  made it possible for student's to determine their house through electronic means. Once
+  you have determined the house you belong to then you can see the qualities of each house,
+  and what majors they are best suited for. We hope that in all of your education concerning
+  magical needs you choose to attend Hogwarts University.
+ </p> -->
+</div>
+
+ <div class = "footer">
+ <footer >
+   <p style = "font-size: 15px"><b>Like what you see? Like us on Facebook or Twitter</b></p>
+  <a href="https://www.facebook.com/"> <img src = "images/facebook.png" alt = "facebook" class = "icons"></a>
+  <a href= "https://twitter.com/?lang=en" ><img src = "images/Twitter.png" alt = "Twitter" class = "icons"></a>
+ </footer>
+</div>
+</div>
 
 </body>
 </html>
