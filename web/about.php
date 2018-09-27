1d4fcbd36c3d86556afcb67fd4544768d187a0c2
Rachel Chang <rjchang96@gmail.com>
2018-09-27 15:45:30 -0600

    about.php

---
+++ web/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>About us</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<header>Destroy your own Mountatin, Inc.</header>
+
+
+</body>
+</html>
