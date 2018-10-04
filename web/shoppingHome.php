e4cccad47943095388082f02ff2e2e9bb6d78409
Rachel Chang <rjchang96@gmail.com>
2018-10-04 13:48:06 -0600

    things

--- web/shoppingHome.php
+++ web/shoppingHome.php
@@ -43,6 +43,44 @@
     <div class="container">
 
       <h1>Cha Chang Foods</h1>
+
+      <div class="container">
+  <h2>Carousel Example</h2>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="blueberries.jpg" alt="Blueberries" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="steak.jpg" alt="Steak" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="frenchBread.jpg" alt="French Bread" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
       <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
 
     </div> <!-- /container -->
