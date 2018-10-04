ab7466175033c821d86d87fd2b5cbb57c019f8db
Rachel Chang <rjchang96@gmail.com>
2018-10-04 16:04:35 -0600

    things

--- web/sessions.php
+++ web/sessions.php
@@ -1,5 +1,6 @@
 <?php
-setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+//setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+$favorite = $_COOKIE["fav-text"];
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,6 +9,8 @@ setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
 </head>
 <body>
    <p>This is a page </p>
-
+<?php
+echo "Your favorite " + $favorite;
+?>
 </body>
 </html>
