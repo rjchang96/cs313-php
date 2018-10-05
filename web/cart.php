f6ec365c64a588fe1a5b7f2691cd6f6e45f93f17
Rachel Chang <rjchang96@gmail.com>
2018-10-05 12:25:46 -0600

    more stuff

--- web/cart.php
+++ web/cart.php
@@ -62,10 +62,10 @@ session_start();
          {
             switch ($key) {
                case 000:
+                  // echo "<br><img src = 'eggplant.jpg' alt = 'eggplant'><br><br>";
+                  // echo "<form action = 'addFood.php' method = 'POST' style =' margin-left: 7%; position: relative;'> <input type='submit' value='+'><input type='hidden' name='item' value = '001'></form>";
+                  //  echo "<form action = 'minusFood.php' method = 'POST' style =' margin-left: 10%; position: relative;'> <input type='submit' value='-'><input type='hidden' name='item' value = '001'></form>";
                   echo "<br><br>Eggplant: $value Cost: " + $value*1.23;
-                  echo "<br><img src = 'eggplant.jpg' alt = 'eggplant'><br><br>";
-                  echo "<form action = 'addFood.php' method = 'POST' style =' margin-left: 7%; position: relative;'> <input type='submit' value='+'><input type='hidden' name='item' value = '001'></form>";
-                   echo "<form action = 'minusFood.php' method = 'POST' style =' margin-left: 10%; position: relative;'> <input type='submit' value='-'><input type='hidden' name='item' value = '001'></form>";
 
                   break;
                   // case 001:
