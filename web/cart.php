339986607def9c1c4e08831710dfeb726456d4de
Rachel Chang <rjchang96@gmail.com>
2018-10-05 10:50:19 -0600

    things

--- web/cart.php
+++ web/cart.php
@@ -7,8 +7,8 @@ session_start();
 <html>
 <head>
    <meta charset="utf-8">
-   <meta name="viewport"
-   content="width=device-width, initial scale=1">
+  <!--  <meta name="viewport"
+   content="width=device-width, initial scale=1"> -->
    <!-- Latest compiled and minified CSS -->
    <link rel = "style" href = "meiDongxi.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
