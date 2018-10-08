a74ae1e400ff6d5392a227f4425611607959a59c
Rachel Chang <rjchang96@gmail.com>
2018-10-08 16:13:35 -0600

    stuff

--- web/cart.php
+++ web/cart.php
@@ -68,6 +68,7 @@ session_start();
                if($value != 0)
                {
                   $cost = 1.23 * $value;
+                   echo "<br><img src = 'eggplant.jpg' alt = 'eggplant'><br><br>";
                   echo "<form action = 'addFood.php' method = 'POST' style =' margin-left: 7%; position: relative;'> <input type='submit' value='+'><input type='hidden' name='item' value = '001'></form>" + "<form action = 'minusFood.php' method = 'POST' style =' margin-left: 10%; position: relative;'> <input type='submit' value='-'><input type='hidden' name='item' value = '001'></form>";
                    // echo "You have purchased $value Eggplants. The Cost is: $cost";
                }
