cd930e8200c5506dfab451f47c62832c4cee9eba
Rachel Chang <rjchang96@gmail.com>
2018-10-09 11:37:41 -0600

    stuff

--- web/checkout.php
+++ web/checkout.php
@@ -112,7 +112,7 @@ session_start();
   </div>
 </div> -->
 
-      <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+     <!--  <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p> -->
 
     </div> <!-- /container -->
 
--- web/handle.php
+++ web/handle.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-$text =
+header("location: cart.php");
 
 ?>
