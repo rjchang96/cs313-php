e06181b27f7a804df69bf230be63b882bd58f2a5
Rachel Chang <rjchang96@gmail.com>
2018-10-13 22:48:07 -0600

    things

--- db/dogs.sql
+++ db/dogs.sql
@@ -8,6 +8,25 @@ CREATE TABLE breeds
    , foodCost  INT NOT NULL
    , size VARCHAR(50) NOT NULL
    , exercise INT NOT NULL
+   , professionalRating INT NOT NULL
+   , userRating INT NOT NULL
 
 
 );
+
+CREATE TABLE users
+(
+   id SERIAL PRIMARY KEY
+   , username VARCHAR(50) UNIQUE NOT NULL
+   , password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE comments
+(
+   id SERIAL PRIMARY KEY
+   , content TEXT
+   , dog_id INT NOT NULL REFERENCES dog(id)
+   , user_id INT NOT NULL REFERENCES users(id)
+
+);
+
