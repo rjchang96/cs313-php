86e64f11a1982add57a749ff99727fc1a4b94a09
Rachel Chang <rjchang96@gmail.com>
2018-10-26 17:22:38 -0600

    things

--- db/dogs.sql
+++ db/dogs.sql
@@ -12,6 +12,7 @@ CREATE TABLE breeds
    , P_hair INT NOT NULL
    , professionalRating INT NOT NULL
    , overallUserRating INT
+   , sizeRange INT NOT NULL
 );
 
 CREATE TABLE users
@@ -44,24 +45,23 @@ CREATE TABLE userRating
 
 INSERT INTO breeds (name, description, P_temperment,
    hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
-   professionalRating) VALUES ('Bichon Frise',
+   professionalRating, sizeRange) VALUES ('Bichon Frise',
    'The small but sturdy and resilient Bichon Frise stands
    among the world’s great “personality dogs.”
    Since antiquity, these irresistible canine
    comedians have relied on charm, beauty, and
    intelligence to weather history’s ups
    and downs.', 9, true, 650, 1500,'9.5-11.5 inches, 12-18 pounds',
-    3, 2, 8);
+    1, 1, 8, 1);
 
    INSERT INTO breeds (name, description, P_temperment,
    hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
-   professionalRating) VALUES ('Siberian Husky',
+   professionalRating, sizeRange) VALUES ('Siberian Husky',
    'The Siberian Husky, a thickly coated, compact sled dog of medium
    size and great endurance, was developed to work in packs, pulling
    light loads at moderate speeds over vast frozen expanses.
    Sibes are friendly, fastidious, and dignified.',
     8, false, 450, 1500,'9.5-11.5 inches, 12-18 pounds',
-    8, 9, 8);
+    5, 5, 8, 3);
 
-   SELECT name, P_temperment, professionalRating FROM breeds
-   WHERE hyperAllergenic = true;
+   SELECT name, P_temperment, professionalRating FROM breeds;
