332663de15e038c99481686171828aefbd60c310
Rachel Chang <rjchang96@gmail.com>
2018-10-31 19:51:17 -0600

    edits

--- web/project/results.php
+++ web/project/results.php
@@ -237,22 +237,22 @@ $list = array();
             //echo "overall dog result: $result <br>";
             $tot = abs((($uTot - $result)/$uTot)) * 100;
             //echo "the percent difference: $tot <br>";
-             // $data = new compare($dname, $ddescription,
-             //   $dsize, $dhair, $dtemperment, $dallergy,
-             //   $dmin, $dmax, $dexercise, $PR, $tot);
+             $data = new compare($dname, $ddescription,
+               $dsize, $dhair, $dtemperment, $dallergy,
+               $dmin, $dmax, $dexercise, $PR, $tot);
             // echo "datas temperment: $data->$temperment<br>";
-            $data = new compare();
-           $data->$pDiff = $tot;
-      $data->$name = $dname;
-     // echo "assignment of compare obj: " . $data->$name ."<br>";
-      $data->$allergy = $dallergy;
-      $data->$description = $ddescription;
-      $data->$temperment = $dtemperment;
-      $data->$size = $dsize;
-      $data->$hair = $dhair;
-      $data->$exercise = $dexercise;
-      $data->$maxcost = $dmax;
-      $data->$mincost = $dmin;
+     //        $data = new compare();
+     //       $data->$pDiff = $tot;
+     //  $data->$name = $dname;
+     // // echo "assignment of compare obj: " . $data->$name ."<br>";
+     //  $data->$allergy = $dallergy;
+     //  $data->$description = $ddescription;
+     //  $data->$temperment = $dtemperment;
+     //  $data->$size = $dsize;
+     //  $data->$hair = $dhair;
+     //  $data->$exercise = $dexercise;
+     //  $data->$maxcost = $dmax;
+     //  $data->$mincost = $dmin;
              echo "datas name: " . $data->getName() . "<br>";
             array_push($list, $data);
             // $list[] = $data;
