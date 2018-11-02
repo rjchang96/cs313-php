ebe8f4a7d11b383f22e6207c735f3327c95ea3e1
Rachel Chang <rjchang96@gmail.com>
2018-11-01 18:35:44 -0600

    tester

--- web/project/test.php
+++ web/project/test.php
@@ -54,8 +54,9 @@ function putStuffIn()
    echo "<h1>in the begining</h1><br>";
    $stuff = new food();
    //$stuff->$category = "fat";
-   $stuff->$name = "pizza";
+  // $stuff->$name = "pizza";
    $stuff->$recipe = "make beautifulness";
+   $stuff->setName("pizza");
    echo "Stuff: " . $stuff->$name . " <br>";
    echo "stuff recipe: " . $stuff->$recipe . "<br>";
    echo "stuf 2: ". $stuff->getName() . " <br>";
