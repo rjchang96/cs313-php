b05ba24f59f3eed372f46cf5c3787729c70212c4
Rachel Chang <rjchang96@gmail.com>
2018-11-06 11:36:30 -0700

    things

--- db/dogs.sql
+++ db/dogs.sql
@@ -87,3 +87,101 @@ INSERT INTO breeds (name, description, P_temperment,
    FROM audienceRating
    INNER JOIN breeds
    ON audienceRating.breeds_id=breeds.id;
+
+   INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Cocker Spaniel',
+   'The merry and frolicsome Cocker Spaniel, with his big,
+   dreamy eyes and impish personality, is one of the world’s
+   best-loved breeds. They were developed as hunting dogs,
+   but Cockers gained their wide popularity as all-around
+   companions.',
+    9, false, 375, 600,'13.5-15.5 inches, 20-30 pounds',
+    3, 3, 8, 2);
+
+   INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Cavalier King Charles Spaniel',
+   'The Cavalier King Charles Spaniel wears his connection to British history
+    in his breed’s name. Cavaliers are the best of two worlds,
+    combining the gentle attentiveness of a toy breed with the
+    verve and athleticism of a sporting spaniel.',
+    9, false, 600, 950,'12-13 inches, 13-18 pounds',
+    2, 3, 9, 1);
+
+
+   INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Labrador',
+   'The sweet-faced, lovable Labrador Retriever is America’s most
+   popular dog breed. Labs are friendly, outgoing,
+   and high-spirited companions who have more than
+   enough affection to go around for a family looking
+   for a medium-to-large dog.',
+    8, false, 500, 4000,'22.5-24.5 inches, 55-80 pounds',
+    4, 3, 9, 3);
+
+    INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Pit Bull',
+   'The Pit Bull immediately strikes one as being a dog of power,
+   passion and undying willingness. The brick-like head,
+   which is especially broad between the cheeks
+   (to house the powerful jaws), is carried upon a
+   thickly muscled, well-defined neck. The neck runs
+   into a deep, thick, well-sprung chest.
+   The American Pit Bull is a very muscular, stocky,
+   yet agile dog that is extremely strong for his size.
+   The tail tapers to a point. The ears are generally
+   cropped, though this is optional.',
+    7, false, 800, 1300,'14-24 inches, 22-78 pounds',
+    5, 2, 7, 3);
+
+    INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Chihuahua',
+   'The Chihuahua is a tiny dog with a huge personality.
+   A national symbol of Mexico, these alert and amusing
+   "purse dogs" stand among the oldest breeds of the Americas,
+   with a lineage going back to the ancient kingdoms of
+   pre-Columbian times.',
+    2, false, 400, 1200,'5-8 inches, less than 6 pounds',
+    3, 1, 4, 1);
+
+   INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Basset Hound',
+   'Among the most appealing of the AKC breeds, the endearing and
+   instantly recognizable Basset Hound is a perennial favorite
+    of dog lovers all over the world. This low-slung and
+    low-key hound can be sometimes stubborn, but is always
+    charming.',
+    10, false, 800, 1700,'up to 15 inches, 40-65 pounds',
+    1, 2, 9, 2);
+
+
+   INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Barbet',
+   'An archetypic water dog of France, the Barbet is a rustic
+   breed of medium size and balanced proportions who appears
+   in artwork as early as the 16th century. In profile, the
+   Barbet is slightly rectangular with a substantial head and
+   long, sweeping tail. He has a long, dense covering of
+   curly hair and a distinctive beard. An agile athlete,
+   the Barbet has been used primarily to locate, flush, and
+   retrieve birds. He has a cheerful disposition and is very
+   social and loyal.',
+    8, true, 1400, 2500,'19-24.5 inches, 35-65 pounds',
+    3, 2, 7, 2);
+
+   INSERT INTO breeds (name, description, P_temperment,
+   hyperAllergenic, minCost, maxCost, size, P_exercise, P_hair,
+   professionalRating, sizeRange) VALUES ('Giant Schnauzer',
+   'The Giant Schnauzer is a larger and more powerful version of
+   the Standard Schnauzer, and he should, as the breed standard
+   says, be a “bold and valiant figure of a dog.”
+   Great intelligence and loyalty make him a stellar worker
+   and companion.',
+    8, true, 400, 1600,'25.5-27.5 inches, 55-75 pounds',
+    4, 3, 7, 2);
